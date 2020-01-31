20dcd521bed254ee7c5bfa9272e3969db95ac99e
UMULISA12 <umulisaa0@gmail.com>
2020-01-31 11:41:05 -0500

    installing body parser middleware

--- assessment.js
+++ assessment.js
@@ -5,3 +5,13 @@ const port = 3000;
 
 // Where we will keep products
 let products = [];
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    // We will be coding here
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
